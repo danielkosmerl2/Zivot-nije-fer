48afe370922e6b35ce1c5c9bd894c902851ddd7e
Jednina <kolobaricnina1@gmail.com>
2025-01-02 20:32:35 +0100

    Start Menu v1
    Napravljen main menu scene, gumb igraj vodi do scene Level 1

---
+++ Assets/Scripts/Navigator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigator : MonoBehaviour
+{
+    public void goToLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
