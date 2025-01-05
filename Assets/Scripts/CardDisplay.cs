e2d523461f5ccc84804774365b8604adb12f90f1
NikoVidovic <148124329+NikoVidovic@users.noreply.github.com>
2025-01-05 02:12:14 +0100

    Game scene
    Dodan dizajn karti.
    Omoguceno klikanje karti i vraćanje nakon klika.
    Karte generirane s pravim artom.

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,9 +8,9 @@ public class CardDisplay : MonoBehaviour
 {
     public Card card;
 
-    public Text nameText;
-    public Text descriptionText;
-    public Text costText;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI costText;
 
     public Image artworkImage;
 
